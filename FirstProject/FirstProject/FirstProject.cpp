237f65014e349c5dd8297bc3007751d0484a6b07
essa <essa@gmail.com>
2022-09-28 09:54:40 +0200

    .

---
+++ FirstProject/FirstProject/FirstProject.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    std::cout << -56;
+    std::cout << "\n";
+    std::cout << "56";
+    std::cout << "\n";
+    std::cout << +15.5;
+    std::cout << "\n";
+
+
+
+    std::cout << "Podaj liczbe:\n";
+    int numberFromUser;
+    std::cin >> numberFromUser;
+    std::cout << "Podano:" << numberFromUser << "\n";
+}
