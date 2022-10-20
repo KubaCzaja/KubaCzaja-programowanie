a994ccef3d8ffb3e724cb31b8111758802910a56
essa <essa@gmail.com>
2022-10-20 15:12:03 +0200

    .

--- FirstProject/DecisionInstruction/DecisionInstruction.cpp
+++ FirstProject/DecisionInstruction/DecisionInstruction.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 /*
-Napisz funkcjê, która wczyta dwie liczby i wykona dzielenie na nich.
+DRY - don't repeat youself - nie powtarzaj siê
 */
 
 void task1()
@@ -19,11 +19,21 @@ void task1()
     // iloraz
     int  quotient = 0;
 
-    if (secondNumber !=0)
+/*    if (secondNumber != 0)
         quotient = firstNumber / secondNumber;
     
-    std::cout << "Iloraz: " << quotient << "\n";
- 
+    if (secondNumber !=0)
+        std::cout << "Iloraz: " << quotient << "\n";
+
+    if (secondNumber == 0)
+        std::cout << "Dzielenie przez zero!!!\n";
+        */
+
+    if (secondNumber != 0)
+    {
+        quotient = firstNumber / secondNumber;
+        std::cout << "Iloraz: " << quotient << "\n";
+    }
 }
 int main()
 {
